8223770af620055c2e0af299896da8393f7d79ce
pengchenggang <76526211@qq.com>
2019-12-16 14:12:22 +0800

    vueInit上传代码

---
+++ src/view/home/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '模板名称',
+  props: {},
+  components: {},
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () { },
+  activated () { },
+  mounted () { },
+  beforeDestroy () { }
+}
+</script>
+
+<style lang="less">
+</style>
